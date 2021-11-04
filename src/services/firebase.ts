9fae2c0511fc81d15713711a254e4318a104a926
Suesley Sunnie <suesleysunnie@hotmail.com>
2021-11-04 14:25:49 -0300

    Second Lesson

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,6 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+import '../styles/global.scss';
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -15,5 +17,7 @@ const firebaseConfig = {
 
   firebase.initializeApp(firebaseConfig);
 
-  export const auth = firebase.auth();
-  export const database = firebase.database();
+  const auth = firebase.auth();
+  const database = firebase.database();
+
+  export { firebase, auth, database };
